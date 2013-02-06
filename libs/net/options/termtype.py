629f54039e38faf2c187e9ff2d6ffec7363a87c7
Eric Davis <endavis@gmail.com>
2013-02-06 20:44:02 +0000

    termtype.py  - new: added ability to negotiate terminal type

---
+++ libs/net/options/termtype.py
@@ -0,0 +1,49 @@
+"""
+$Id$
+"""
+from libs.net.options._option import TelnetOption
+from libs.net.telnetlib import WILL, DO, IAC, SE, SB, DONT
+
+
+TTYPE = chr(24)  # Terminal Type
+
+canreload = False
+
+
+class SERVER(TelnetOption):
+  def __init__(self, telnetobj):
+    TelnetOption.__init__(self, telnetobj, TTYPE)
+    #self.telnetobj.debug_types.append('TTYPE')
+
+  def handleopt(self, command, sbdata):
+    self.telnetobj.msg('TTYPE:', ord(command), '- in handleopt', mtype='TTYPE')
+    if command == DO:
+      self.telnetobj.msg('TTYPE: sending IAC SB TTYPE NOOP MUSHclient-Aard IAC SE', mtype='TTYPE')
+      self.telnetobj.send(IAC + SB + TTYPE + chr(0) + self.telnetobj.ttype + IAC + SE)
+
+
+class CLIENT(TelnetOption):
+  def __init__(self, telnetobj):
+    TelnetOption.__init__(self, telnetobj, TTYPE)
+    #self.telnetobj.debug_types.append('TTYPE')
+    self.telnetobj.msg('TTYPE: sending IAC WILL TTYPE', mtype='TTYPE')
+    self.telnetobj.addtooutbuffer(IAC + DO + TTYPE, True)
+
+  def handleopt(self, command, sbdata):
+    self.telnetobj.msg('TTYPE:', ord(command), '- in handleopt: ', sbdata, mtype='TTYPE')
+
+    if command == WILL:
+      self.telnetobj.addtooutbuffer(IAC + SB + TTYPE +  chr(1)  + IAC + SE, True)
+    elif command == SE:
+      self.telnetobj.ttype = sbdata.strip()
+
+  def negotiate(self):
+    self.telnetobj.msg("TTYPE: starting TTYPE", level=2, mtype='TTYPE')
+    self.telnetobj.msg('TTYPE: sending IAC SB TTYPE IAC SE', mtype='TTYPE')
+    self.telnetobj.send(IAC + SB + TTYPE + IAC + SE)
+
+  def reset(self, onclose=False):
+    self.telnetobj.msg('TTYPE: resetting', mtype='TTYPE')
+    if not onclose:
+      self.telnetobj.addtooutbuffer(IAC + DONT + TTYPE, True)    
+    TelnetOption.reset(self)  
