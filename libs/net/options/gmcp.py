79fe2f603cfbab9884a9feff0d37bc0df40ec5e4
Eric Davis <endavis@gmail.com>
2013-08-05 00:59:15 +0000

    gmcp.py  - new: add a message to show gmcp data

--- libs/net/options/gmcp.py
+++ libs/net/options/gmcp.py
@@ -14,17 +14,17 @@ GMCP:<base module name>
 GMCP:<full module name>
 
 The args for the event will look like
-{'data': {u'clan': u'', u'name': u'Bast', u'perlevel': 6000, 
-          u'remorts': 1, u'subclass': u'Ninja', u'race': u'Shadow', 
-          u'tier': 6, u'class': u'Thief', u'redos': u'0', u'pretitle': u''}, 
+{'data': {u'clan': u'', u'name': u'Bast', u'perlevel': 6000,
+          u'remorts': 1, u'subclass': u'Ninja', u'race': u'Shadow',
+          u'tier': 6, u'class': u'Thief', u'redos': u'0', u'pretitle': u''},
  'module': 'char.base'}
 
 It adds the following functions to exported
 
 gmcp.get(module) - get data that is in cache for the specified gmcp module
-gmcp.sendpacket(what) - send a gmcp packet to 
+gmcp.sendpacket(what) - send a gmcp packet to
                 the mud with the specified contents
-gmcp.togglemodule(modname, mstate) - toggle the gmcp module 
+gmcp.togglemodule(modname, mstate) - toggle the gmcp module
                 with modname, mstate should be True or False
 
 To get GMCP data:
@@ -49,7 +49,7 @@ VERSION = 1
 
 AUTOLOAD = True
 
-    
+
 #IAC SB GMCP <gmcp message text> IAC SE
 def gmcpsendpacket(what):
   """
@@ -57,10 +57,10 @@ def gmcpsendpacket(what):
   only argument is what to send
   """
   exported.event.eraise('to_mud_event', {'data':'%s%s%s%s%s%s' % \
-              (IAC, SB, GMCP, what.replace(IAC, IAC+IAC), IAC, SE), 
-              'raw':True, 'dtype':GMCP})  
-    
-    
+              (IAC, SB, GMCP, what.replace(IAC, IAC+IAC), IAC, SE),
+              'raw':True, 'dtype':GMCP})
+
+
 # Server
 class SERVER(TelnetOption):
   """
@@ -77,22 +77,22 @@ class SERVER(TelnetOption):
     """
     handle the gmcp option
     """
-    self.telnetobj.msg('GMCP:', ord(command), '- in handleopt', 
+    self.telnetobj.msg('GMCP:', ord(command), '- in handleopt',
                               level=2, mtype='GMCP')
     if command == WILL:
       self.telnetobj.msg('GMCP: sending IAC DO GMCP', level=2, mtype='GMCP')
       self.telnetobj.send(IAC + DO + GMCP)
       self.telnetobj.options[ord(GMCP)] = True
       exported.event.eraise('GMCP:server-enabled', {})
-      
+
     elif command == SE:
       if not self.telnetobj.options[ord(GMCP)]:
         # somehow we missed negotiation
-        self.telnetobj.msg('##BUG: Enabling GMCP, missed negotiation', 
+        self.telnetobj.msg('##BUG: Enabling GMCP, missed negotiation',
                                                   level=2, mtype='GMCP')
-        self.telnetobj.options[ord(GMCP)] = True        
+        self.telnetobj.options[ord(GMCP)] = True
         exported.event.eraise('GMCP:server-enabled', {})
-        
+
       data = sbdata
       modname, data = data.split(" ", 1)
       try:
@@ -108,8 +108,8 @@ class SERVER(TelnetOption):
       tdata['module'] = modname
       tdata['server'] = self.telnetobj
       exported.event.eraise('to_client_event', {'todata':'%s%s%s%s%s%s' % \
-                      (IAC, SB, GMCP, sbdata.replace(IAC, IAC+IAC), IAC, SE), 
-                      'raw':True, 'dtype':GMCP})      
+                      (IAC, SB, GMCP, sbdata.replace(IAC, IAC+IAC), IAC, SE),
+                      'raw':True, 'dtype':GMCP})
       exported.event.eraise('GMCP_raw', tdata)
 
 
@@ -123,25 +123,25 @@ class CLIENT(TelnetOption):
     initalize the instance
     """
     TelnetOption.__init__(self, telnetobj, GMCP)
-    #self.telnetobj.debug_types.append('GMCP')    
-    self.telnetobj.msg('GMCP: sending IAC WILL GMCP', mtype='GMCP')    
+    #self.telnetobj.debug_types.append('GMCP')
+    self.telnetobj.msg('GMCP: sending IAC WILL GMCP', mtype='GMCP')
     self.telnetobj.addtooutbuffer(IAC + WILL + GMCP, True)
     self.cmdqueue = []
-    
+
   def handleopt(self, command, sbdata):
     """
     handle gmcp data from a client
     """
     self.telnetobj.msg('GMCP:', ord(command), '- in handleopt', mtype='GMCP')
     if command == DO:
-      self.telnetobj.msg('GMCP:setting options["GMCP"] to True', 
-                                    mtype='GMCP')    
-      self.telnetobj.options[ord(GMCP)] = True        
+      self.telnetobj.msg('GMCP:setting options["GMCP"] to True',
+                                    mtype='GMCP')
+      self.telnetobj.options[ord(GMCP)] = True
     elif command == SE:
-      exported.event.eraise('GMCP_from_client', 
+      exported.event.eraise('GMCP_from_client',
                       {'data': sbdata, 'client':self.telnetobj})
-      
-      
+
+
 # Plugin
 class Plugin(BasePlugin):
   """
@@ -150,12 +150,12 @@ class Plugin(BasePlugin):
   def __init__(self, *args, **kwargs):
     """
     Iniitialize the class
-    
+
     self.gmcpcache - the cache of values for different GMCP modules
     self.modstates - the current counter for what modules have been enabled
-    self.gmcpqueue - the queue of gmcp commands that the client sent 
+    self.gmcpqueue - the queue of gmcp commands that the client sent
               before connected to the server
-    self.gmcpmodqueue - the queue of gmcp modules that were enabled by 
+    self.gmcpmodqueue - the queue of gmcp modules that were enabled by
               the client before connected to the server
     """
     BasePlugin.__init__(self, *args, **kwargs)
@@ -166,22 +166,22 @@ class Plugin(BasePlugin):
     self.events['GMCP_raw'] = {'func':self.gmcpfromserver}
     self.events['GMCP_from_client'] = {'func':self.gmcpfromclient}
     self.events['GMCP:server-enabled'] = {'func':self.gmcprequest}
-    self.events['muddisconnect'] = {'func':self.disconnect}     
+    self.events['muddisconnect'] = {'func':self.disconnect}
     self.canreload = False
 
     self.gmcpcache = {}
     self.modstates = {}
     self.gmcpqueue = []
-    self.gmcpmodqueue = []   
-    
-    self.reconnecting = False   
+    self.gmcpmodqueue = []
+
+    self.reconnecting = False
 
   def disconnect(self, _=None):
     """
     disconnect
     """
     self.msg('setting reconnect to true')
-    self.reconnecting = True    
+    self.reconnecting = True
 
   def gmcptogglemodule(self, modname, mstate):
     """
@@ -191,41 +191,41 @@ class Plugin(BasePlugin):
     """
     if not (modname in self.modstates):
       self.modstates[modname] = 0
-    
+
     if mstate:
       if self.modstates[modname] == 0:
         self.msg('Enabling GMCP module: %s' % modname)
         cmd = 'Core.Supports.Set [ "%s %s" ]' % (modname, 1)
         gmcpsendpacket(cmd)
       self.modstates[modname] = self.modstates[modname] + 1
-      
+
     else:
       self.modstates[modname] = self.modstates[modname] - 1
       if self.modstates[modname] == 0:
         self.msg('Disabling GMCP module: %s' % modname)
         cmd = 'Core.Supports.Set [ "%s %s" ]' % (modname, 0)
-        gmcpsendpacket(cmd)       
-    
+        gmcpsendpacket(cmd)
+
   def gmcpget(self, module):
     """
     Get a gmcp module from the cache
     argument 1: module name (such as char.status)
     """
-    mods = module.split('.')  
+    mods = module.split('.')
     mods = [x.lower() for x in mods]
     tlen = len(mods)
-      
+
     currenttable = self.gmcpcache
     #previoustable = DotDict()
     for i in range(0, tlen):
       if not (mods[i] in currenttable):
         return None
-      
+
       #previoustable = currenttable
       currenttable = currenttable[mods[i]]
-      
+
     return currenttable
-    
+
   def sendmoduletoclients(self, modname):
     """
     send a gmcp module
@@ -236,49 +236,49 @@ class Plugin(BasePlugin):
       tdata = json.dumps(data)
       tpack = '%s %s' % (modname, tdata)
       exported.event.eraise('to_client_event', {'todata':'%s%s%s%s%s%s' % \
-              (IAC, SB, GMCP, tpack.replace(IAC, IAC+IAC), IAC, SE), 
+              (IAC, SB, GMCP, tpack.replace(IAC, IAC+IAC), IAC, SE),
               'raw':True, 'dtype':GMCP})
-    
+
   def gmcpfromserver(self, args):
     """
     handle gmcp data from the server
     """
     modname = args['module'].lower()
-    mods = modname.split('.')  
-    mods = [x.lower() for x in mods]    
+    mods = modname.split('.')
+    mods = [x.lower() for x in mods]
 
     if modname != 'room.wrongdir':
       tlen = len(mods)
-        
+
       currenttable = self.gmcpcache
       previoustable = DotDict()
       for i in range(0, tlen):
         if not (mods[i] in currenttable):
           currenttable[mods[i]] = DotDict()
-        
+
         previoustable = currenttable
         currenttable = currenttable[mods[i]]
-        
-      previoustable[mods[tlen - 1]] = DotDict()  
+
+      previoustable[mods[tlen - 1]] = DotDict()
       datatable = previoustable[mods[tlen - 1]]
-      
+
       for i in args['data']:
         try:
           datatable[i] = args['data'][i]
         except TypeError:
           msg = []
           msg.append("TypeError: string indices must be integers")
-          msg.append('i: %s' % i) 
+          msg.append('i: %s' % i)
           msg.append('datatable: %s' % datatable)
           msg.append('args: %s' % args)
           msg.append('args[data]: %s' % args['data'])
           exported.write_traceback('\n'.join(msg))
 
-    
+    self.msg('%s : %s' % (args['module'], args['data']))
     exported.event.eraise('GMCP', args)
     exported.event.eraise('GMCP:%s' % modname, args)
     exported.event.eraise('GMCP:%s' % mods[0], args)
-    
+
   def gmcprequest(self, _=None):
     """
     handle a gmcp request
@@ -294,12 +294,12 @@ class Plugin(BasePlugin):
         if tnum > 0:
           self.msg('Re-Enabling GMCP module %s' % i)
           cmd = 'Core.Supports.Set [ "%s %s" ]' % (i, 1)
-          gmcpsendpacket(cmd)        
-        
+          gmcpsendpacket(cmd)
+
     for i in self.gmcpqueue:
       gmcpsendpacket(i)
     self.gmcpqueue = []
-  
+
   def gmcpfromclient(self, args):
     """
     handle gmcp data from the client
@@ -316,7 +316,7 @@ class Plugin(BasePlugin):
           toggle = True
         else:
           toggle = False
-            
+
         if not exported.CONNECTED:
           self.gmcpmodqueue.append({'modname':modname, 'toggle':toggle})
         else:
@@ -324,12 +324,12 @@ class Plugin(BasePlugin):
     elif 'rawcolor' in data.lower() or 'group' in data.lower():
       #we only support rawcolor on right now, the json parser doesn't like
       #ascii codes, we also turn on group and leave it on
-      return          
+      return
     else:
       if not exported.CONNECTED:
         if not (data in self.gmcpqueue):
           self.gmcpqueue.append(data)
       else:
-        gmcpsendpacket(data)  
-   
-  
+        gmcpsendpacket(data)
+
+
