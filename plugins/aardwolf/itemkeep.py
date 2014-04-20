716eb3ada3542642865a693942c94d4435694a5f
Eric Davis <endavis@gmail.com>
2014-04-20 11:29:22 -0400

    new plugin to automatically keep certain item type

---
+++ plugins/aardwolf/itemkeep.py
@@ -0,0 +1,47 @@
+"""
+$Id$
+
+This plugin is a utility plugin for aardwolf functions
+It adds functions to exported.aardu
+"""
+from plugins.aardwolf._aardwolfbaseplugin import AardwolfBasePlugin
+
+NAME = 'Aardwolf Item autokeep'
+SNAME = 'itemkeep'
+PURPOSE = 'keep an item type automatically'
+AUTHOR = 'Bast'
+VERSION = 1
+
+AUTOLOAD = False
+
+class Plugin(AardwolfBasePlugin):
+  """
+  a plugin to handle aardwolf cp events
+  """
+  def __init__(self, *args, **kwargs):
+    AardwolfBasePlugin.__init__(self, *args, **kwargs)
+
+  def load(self):
+    """
+    load the plugins
+    """
+    AardwolfBasePlugin.load(self)
+
+    itemtypes = self.api.get('itemu.objecttypes')()
+
+    for i in itemtypes:
+      self.api.get('setting.add')(i, False, bool,
+                                    'autokeep %s' % i)
+
+    self.api.get('events.register')('inventory_added', self.inventory_added)
+
+  def inventory_added(self, args):
+    """
+    check an item added to inventory to autokeep
+    """
+    itemtypesrev = self.api.get('itemu.objecttypes')()
+    ntype = itemtypesrev[args['type']]
+
+    if self.api.get('setting.gets')(ntype):
+      if not ('K' in args['shortflags']):
+        self.api.get('send.execute')('keep %s' % args['serial'])
