1faeb6c4e7a7a418f6d1b7815845ae6e720f0528
Eric Davis <endavis@gmail.com>
2014-09-04 10:46:13 -0400

    add ice age events

---
+++ plugins/aardwolf/iceage.py
@@ -0,0 +1,46 @@
+"""
+This plugin sends emails when certain events happen in aardwolf
+
+It sends alerts for the following:
+
+ * quests available
+ * gq available
+ * ice age
+"""
+from plugins.aardwolf._aardwolfbaseplugin import AardwolfBasePlugin
+
+NAME = 'Aardwolf Ice Age'
+SNAME = 'iceage'
+PURPOSE = 'Send ice age events'
+AUTHOR = 'Bast'
+VERSION = 1
+
+AUTOLOAD = False
+
+class Plugin(AardwolfBasePlugin):
+  """
+  a plugin to handle aardwolf quest events
+  """
+  def __init__(self, *args, **kwargs):
+    """
+    initialize the instance
+    """
+    AardwolfBasePlugin.__init__(self, *args, **kwargs)
+
+  def load(self):
+    """
+    load the plugins
+    """
+    AardwolfBasePlugin.load(self)
+
+    self.api.get('triggers.add')('iceage',
+      "[[ WARNING: An Ice Age Approaches - 1 minute - See 'help ice age' ]]")
+
+    self.api.get('events.register')('iceage',
+                                    self.iceage)
+
+  def iceage(self, args):
+    """
+    raise an iceage event
+    """
+    self.api.get('events.eraise')('aard_iceage', {})
