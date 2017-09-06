3307ab4596cea8907d0ca7615c0862750cbb3c4d
Eric Davis <endavis@gmail.com>
2017-09-06 10:39:57 -0700

    use timeit's default_timer for timing functions

--- libs/timing.py
+++ libs/timing.py
@@ -1,7 +1,7 @@
 """
 this module is for timing functions
 """
-import time
+from timeit import default_timer
 from libs.api import API as BASEAPI
 API = BASEAPI()
 
@@ -13,10 +13,10 @@ def timeit(func):
     """
     the wrapper to time a function
     """
-    time1 = time.time()
+    time1 = default_timer()
     API.get('send.msg')('%s: started %s' % (func.func_name, arg), 'timing')
     res = func(*arg)
-    time2 = time.time()
+    time2 = default_timer()
     API('log.adddtype')('timing')
     API('send.msg')('%s: %0.3f ms' % \
               (func.func_name, (time2-time1)*1000.0), 'timing')
