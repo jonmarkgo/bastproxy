41fede3fbd76f6c804049ebe0b380f74e0db01aa
Eric Davis <endavis@gmail.com>
2017-11-28 09:55:09 -0800

    add a timing class

--- libs/timing.py
+++ libs/timing.py
@@ -13,12 +13,69 @@ def timeit(func):
     """
     the wrapper to time a function
     """
-    time1 = default_timer()
-    API('send.msg')('%s: started %s' % (func.func_name, arg), 'timing')
+    tname = '%s' % (func.func_name)
+    TIMING.starttimer(tname, arg)
     res = func(*arg)
-    time2 = default_timer()
-    API('log.adddtype')('timing')
-    API('send.msg')('%s: %0.3f ms' % \
-              (func.func_name, (time2-time1)*1000.0), 'timing')
+    TIMING.finishtimer(tname, arg)
     return res
   return wrapper
+
+class Timing(object):
+  """
+  manage timing functions
+  """
+  def __init__(self):
+    """
+    create the dictionary
+    """
+    self.api = API
+    self.enabled = True
+
+    self.timing = {}
+
+    self.api('api.add')('timep', 'start', self.starttimer)
+    self.api('api.add')('timep', 'finish', self.finishtimer)
+    self.api('api.add')('timep', 'toggle', self.toggletiming)
+
+  def toggletiming(self, tbool=None):
+    """
+    toggle the timing flag
+    """
+    if tbool is None:
+      self.enabled = not self.enabled
+    else:
+      self.enabled = bool(tbool)
+
+  def starttimer(self, timername, args):
+    """
+    start a timer
+    """
+    if self.enabled:
+      plugin = self.api('api.callerplugin')()
+      self.timing[timername] = {}
+      self.timing[timername]['start'] = default_timer()
+      self.timing[timername]['plugin'] = plugin
+      self.api('send.msg')('%-20s : started - from plugin %s with args %s' % \
+                            (timername, plugin, args),
+                           primary=plugin, secondary='timing')
+
+  def finishtimer(self, timername, args):
+    """
+    finish a timer
+    """
+    if self.enabled:
+      timerfinish = default_timer()
+      if timername in self.timing:
+        self.api('send.msg')('%-20s : finished in %s ms - with args %s' % \
+                             (timername,
+                              (timerfinish - self.timing[timername]['start']) * 1000.0,
+                              args),
+                             primary=self.timing[timername]['plugin'], secondary='timing')
+        del self.timing[timername]
+      else:
+        plugin = self.api('api.callerplugin')()
+        self.api('send.error')('timername: %s not found, called from %s' % \
+                              (timername, plugin),
+                               secondary=['timing', plugin])
+
+TIMING = Timing()
